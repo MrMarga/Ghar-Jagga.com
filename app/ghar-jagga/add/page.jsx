b3dbed43b33ae0acb1ad0cdcca68409e64a9eb36
MrMarga <margaghale31@gmail.com>
2024-03-09 22:54:29 +0545

    Test

---
+++ app/ghar-jagga/add/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Property Add Page</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/ghar-jagga/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Ghar Jagga Page</h1>
+    </div>
+  );
+};
+
+export default page;
