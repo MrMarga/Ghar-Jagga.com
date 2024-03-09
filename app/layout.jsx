14fbd3e3aed75287da4dc275e4cb4c629329d8ad
MrMarga <margaghale31@gmail.com>
2024-03-09 22:35:17 +0545

    add .env file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "@/assets/styles/global.css";
 
+export const metadata = {
+  title: "Ghar-Jagga.com",
+  description: "Buy Or Sell Property Or Rent ",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html lang="en">
