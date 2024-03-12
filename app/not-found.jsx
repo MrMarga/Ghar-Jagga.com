a561863d5f5a06e17d724b996cdef776c8785098
MrMarga <margaghale31@gmail.com>
2024-03-12 16:58:34 +0545

    This reverts commit 584bd1adba8252dd9548be87213230c55cfe533a.

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -5,7 +5,7 @@ import { FaExclamationTriangle } from "react-icons/fa";
 const NotFoundPage = () => {
   return (
     <section className="bg-blue-50 min-h-screen flex-grow">
-      <div className="container max-w-2xl py-29">
+      <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-24 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <div className="flex justify-center">
             <FaExclamationTriangle className="text-8xl text-yellow-400"></FaExclamationTriangle>
